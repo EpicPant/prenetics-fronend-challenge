01e5b48f2750b13ad27ed3ba9a66987396c61448
james_ycy <james.yip@prenetics.com>
2022-02-07 19:19:58 +0800

    added <CircleResultTableRow /> for displaying extra data when organisation is Circle

--- src/component/ResultTableRow.tsx
+++ src/component/ResultTableRow.tsx
@@ -13,3 +13,17 @@ export const ResultTableRow = (props: ResultType) => {
         </tr>
     );
 }
+
+export const CircleResultTableRow = (props: ResultType) => {
+    return (
+        <tr id="results" data-testid='circle-test-result-data'>
+            <td>{props.sampleId}</td>
+            <td>{props.activationTime}</td>
+            <td>{props.resultTime}</td>
+            <td>{props.resultType || '-'}</td>
+            <td>-</td>
+            <td>{props.patientName}</td>
+            <td>{props.result === 'positive' ? 'detected' : 'not-detected'}</td>
+        </tr>
+    )
+}
