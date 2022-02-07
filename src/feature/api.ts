a0929c656dac06aa165cdc8bb8fc85bfdd6f3b77
james_ycy <james.yip@prenetics.com>
2022-02-07 19:01:37 +0800

    api - function call to the backend api

---
+++ src/feature/api.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { RootState } from '../store';
+import { API_URL, ORGANISATION_ID } from '../resources/constants';
+import { ApiResponseType, ResultState } from '../resources/types';
+
+export const fetchResultsFormAPI = (state: RootState) => {
+    let url = `${API_URL}/org/${ORGANISATION_ID}/sample?limit=${state.pagination.limit}&offset=${state.pagination.current_page - 1}&`;
+    const params = new URLSearchParams();
+
+    let k: keyof typeof state.search.query;
+    for (k in state.search.query) {
+        if (state.search.query[k]!.length > 1) {
+            params.append(k, state.search.query[k]!)
+        }
+    }
+    
+    url += params.toString();
+
+    return axios.get(url)
+    .then((res): ResultState => {
+        const { data, meta, included } = res.data as ApiResponseType;
+        return {
+            status: state.result.status,
+            data: data.map((result, i) => {
+                return {
+                    patientName: included[i].attributes.name,
+                    sampleId: result.attributes.sampleId,
+                    result: result.attributes.result || '',
+                    resultType: result.attributes.resultType,
+                    resultTime: new Date(result.attributes.resultTime).toLocaleString(),
+                    activationTime: new Date(result.attributes.activateTime).toLocaleString()
+                }
+            }),
+            meta: {
+                total: meta.total
+            }
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+};
