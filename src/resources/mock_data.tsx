4d9dd232038d34774bc692cf6c942d16b0c6c2b7
james_ycy <james.yip@prenetics.com>
2022-02-07 21:07:11 +0800

    mock data reformatted

--- src/resources/mock_data.tsx
+++ src/resources/mock_data.tsx
@@ -1,10 +1,84 @@
 import { ResultType } from "./types"
 
-export const mockData: ResultType = {
-    sampleId: "213812937246234",
-    patientName: "Peter Chan",
-    activationTime: new Date().toLocaleString(),
-    resultTime: new Date().toLocaleString(),
-    result: 'negative',
-    resultType: 'rtpcr'
-}
+export const mockData: ResultType[] = [
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    },
+    {
+        sampleId: "213812937246234",
+        patientName: "James Yip",
+        activationTime: new Date().toLocaleString(),
+        resultTime: new Date().toLocaleString(),
+        result: 'negative',
+        resultType: 'rtpcr'
+    }
+]
