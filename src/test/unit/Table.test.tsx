cfbe13f6733782eb24de9a9df24fd3d2734fa13b
james_ycy <james.yip@prenetics.com>
2022-02-08 15:24:20 +0800

    tests if correct data is shown with different organisation

--- src/test/unit/Table.test.tsx
+++ src/test/unit/Table.test.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { render, cleanup, screen } from '@testing-library/react';
+import { render, cleanup, screen, queryByText } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../store';
 import { TestResultPage } from '../../component/TestResultPage';
 import { server } from '../../resources/mock_handler';
+import { getResultsFromAPI } from '../../feature/result';
+import { switchOrganisation } from '../../feature/organisation';
 
 describe('Result Table testing', () => {
 
     beforeAll(() => {
-        server.listen();
+        server.listen({ onUnhandledRequest: 'bypass' });
     });
 
     afterAll(() => {
@@ -28,12 +30,26 @@ describe('Result Table testing', () => {
         server.resetHandlers();
     });
 
-    test('test display result correctly', async () => {
+    test('renders loading before making api call', async () => {
         expect(screen.getByText(/Loading/i)).toBeInTheDocument();
-        expect(await screen.findByTestId('test-result-page')).toBeInTheDocument();
-        expect(screen.queryByTestId('test-result-page-loading')).toBeNull();
-        expect(screen.queryAllByTestId('test-result-data').length).toBeGreaterThanOrEqual(0);
-        expect(screen.queryAllByTestId('test-result-data').length).toBeLessThanOrEqual(15);
+        await store.dispatch(getResultsFromAPI())
+        expect(screen.queryByText(/Loading/i)).toBeNull();
     });
 
+    test('renders CircleResultTableRow when logged in as organisation Circle', async () => {
+        const tableRow = await screen.findAllByTestId('circle-test-result-data');
+        const hiddenTableRow = await screen.queryAllByTestId('test-result-data');
+        expect(hiddenTableRow.length).toEqual(0);
+        expect(tableRow.length).toEqual(2);
+    });
+
+    test('renders ResultTableRow when not logged in as organisation Circle', async () => {
+        store.getState().organisation.name !== 'Circle' || store.dispatch(switchOrganisation());
+        const tableRow = await screen.findAllByTestId('test-result-data');
+        const hiddenTableRow = screen.queryAllByTestId('circle-test-result-data');
+        expect(hiddenTableRow.length).toEqual(0);
+        expect(tableRow.length).toEqual(2);
+    });
+
+    
 });
