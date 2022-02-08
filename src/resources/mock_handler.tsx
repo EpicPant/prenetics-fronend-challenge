bd1682c66ce9957fd773c3cc6c8107da7e816a7b
james_ycy <james.yip@prenetics.com>
2022-02-08 15:22:58 +0800

    use mockAPIResponse in msw

--- src/resources/mock_handler.tsx
+++ src/resources/mock_handler.tsx
@@ -1,16 +1,11 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { API_URL, ORGANISATION_ID } from './constants';
-import { mockData } from './mock_data';
+import { mockAPIResponse } from './mock_data';
 
 const handlers = [
-    rest.get(`${API_URL}/org/${ORGANISATION_ID}/sample`, (req, res, ctx) => {
-        const data = [];
-        for (var i = 0; i < 15; i++) {
-            data.push(mockData);
-        }
-
-        return res(ctx.json(data), ctx.delay(150));
+    rest.get(`${API_URL}/org/${ORGANISATION_ID}/sample`, (_, res, ctx) => {
+        return res(ctx.json(mockAPIResponse));
     })
 ]
 
