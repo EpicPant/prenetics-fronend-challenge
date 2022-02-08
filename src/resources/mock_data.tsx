e06a280dcdb3977ba0b928b014ca0ab2dd115f01
james_ycy <james.yip@prenetics.com>
2022-02-08 15:22:44 +0800

    added a mockAPIResponse for the msw response

--- src/resources/mock_data.tsx
+++ src/resources/mock_data.tsx
@@ -1,4 +1,4 @@
-import { ResultType } from "./types"
+import { ApiResponseType, ResultType } from "./types"
 
 export const mockData: ResultType = {
     sampleId: "213812937246234",
@@ -7,4 +7,50 @@ export const mockData: ResultType = {
     resultTime: new Date().toLocaleString(),
     result: 'negative',
     resultType: 'rtpcr'
+}
+
+export const mockAPIResponse: ApiResponseType = {
+    meta: {
+        total: 2
+    },
+    data: [
+        {
+            id: '',
+            type: '',
+            attributes: {
+                resultType: mockData.resultType,
+                sampleId: mockData.sampleId,
+                activateTime: mockData.activationTime,
+                resultTime: mockData.resultTime,
+                result: mockData.result
+            }
+        },
+        {
+            id: '',
+            type: '',
+            attributes: {
+                resultType: mockData.resultType,
+                sampleId: mockData.sampleId,
+                activateTime: mockData.activationTime,
+                resultTime: mockData.resultTime,
+                result: mockData.result
+            }
+        }
+    ],
+    included: [
+        {
+            id: '',
+            type: '',
+            attributes: {
+                name: mockData.patientName
+            }
+        },
+        {
+            id: '',
+            type: '',
+            attributes: {
+                name: mockData.patientName
+            }
+        }
+    ]
 }
