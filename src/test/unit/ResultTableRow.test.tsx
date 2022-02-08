01e603de5b644c9ec1330e7446f56098b137c9ae
james_ycy <james.yip@prenetics.com>
2022-02-08 15:23:34 +0800

    added tests for CircleResultTableRow

--- src/test/unit/ResultTableRow.test.tsx
+++ src/test/unit/ResultTableRow.test.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { render, cleanup, screen } from '@testing-library/react';
-import { ResultTableRow } from '../../component/ResultTableRow';
+import { CircleResultTableRow, ResultTableRow } from '../../component/ResultTableRow';
 import { ResultType } from '../../resources/types';
 
-describe('Result Table Row', () => {
-    const initialValue: ResultType = {
-        sampleId: "123456",
-        activationTime: new Date().toDateString(),
-        resultTime: new Date(new Date().setFullYear(2009)).toDateString(),
-        result: 'negative',
-        resultType: 'rtpcr',
-        patientName: 'Bruce Lee'
-    }
+const initialValue: ResultType = {
+    sampleId: "123456",
+    activationTime: new Date().toDateString(),
+    resultTime: new Date(new Date().setFullYear(2009)).toDateString(),
+    result: 'negative',
+    resultType: 'rtpcr',
+    patientName: 'Bruce Lee'
+}
+
+describe('Result Table Row (Circle)', () => {
+    beforeEach(() => {
+        render(
+            <table>
+                <tbody>
+                    <CircleResultTableRow {...initialValue} />
+                </tbody>
+            </table>
+        )
+    })
+
+    afterAll(cleanup);
+
+    it('should render 6 td', () => {
+        const elm = screen.getByTestId('circle-test-result-data');
+        expect(elm.childElementCount).toEqual(7);
+        expect(screen.getByText(initialValue.sampleId)).toBeTruthy;
+        expect(screen.getByText(initialValue.activationTime)).toBeTruthy;
+        expect(screen.getByText(initialValue.resultType)).toBeTruthy;
+        expect(screen.getByText(initialValue.resultTime)).toBeTruthy;
+        expect(screen.getByText(initialValue.patientName)).toBeTruthy;
+    });
+});
+
+describe('Result Table Row (Non Circle)', () => {
 
     beforeEach(() => {
         render(
@@ -25,7 +50,7 @@ describe('Result Table Row', () => {
 
     afterEach(cleanup);
 
-    it('should render 5 td with corresponding result data', () => {
+    it('should render 5 td', () => {
         const elm = screen.getByTestId('test-result-data');
         expect(elm.childElementCount).toEqual(6);
         expect(screen.getByText(initialValue.sampleId)).toBeTruthy();
